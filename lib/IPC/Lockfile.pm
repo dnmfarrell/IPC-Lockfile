8a85102c707b2118fc82a317f9b10edd223399f6
David Farrell <davidnmfarrell@gmail.com>
2015-10-30 21:02:51 -0400

    Removed need for external lockfile

--- lib/IPC/Lockfile.pm
+++ lib/IPC/Lockfile.pm
@@ -5,12 +5,9 @@ package IPC::Lockfile;
 # ABSTRACT: run only one instance of a program at a time using flock
 use Fcntl qw(:flock);
 
-my $filepath = "$0.lock";
-
 # lexical filehandles dont work!
-open LOCKFILE, '>', $filepath
-  or die "Unable to create the lockfile $filepath $!\n";
-flock LOCKFILE, LOCK_EX|LOCK_NB or die "$0 is running!\n";;
+open LOCKFILE, '<', $0  or die "Unable to create the lockfile $0 $!\n";
+flock LOCKFILE, LOCK_EX|LOCK_NB or die "$0 is running!\n";
 
 =head1 SYNOPSIS
 
@@ -21,20 +18,8 @@ Just import the module:
 
     ... # program code here
 
-This will create a lockfile in the same directory as the program. To avoid
-race conditions, the file will not be deleted when the program ends.
-
-Or do not use this module and just add these lines to your program:
-
-  use Fcntl qw(:flock);
-  flock DATA, LOCK_EX|LOCK_NB or die "$0 is running!\n";;
-
-  ... # program code goes here
-
-  __DATA__
-
-No external lockfile is created as it uses C<_DATA_>. An elegant L<solution|http://perl.plover.com/yak/flock/samples/slide006.html>
-for lockfiles proposed by Mark Jason Dominus.
+This will open a lock on the program file, avoiding the need for an external
+lockfile. This elegant L<solution|http://perl.plover.com/yak/flock/samples/slide006.html> for lockfiles was proposed by Mark Jason Dominus.
 
 =head1 DESCRIPTION
 
